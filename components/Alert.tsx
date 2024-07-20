2328b3d3ae85b4a3ed50b4e2d4d16acc8a44b86a
ddoemonn <ozergokalpsezer@gmail.com>
2024-07-20 17:44:43 +0300

    ADD: alert component

---
+++ components/Alert.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+interface AlertProps {
+  variant?: 'white' | 'filled';
+  color?: 'cyan' | 'red' | 'green' | 'yellow';
+  title: string;
+  icon: React.ReactElement;
+  children: React.ReactNode;
+}
+
+const baseStyles = 'p-4 flex items-start';
+
+const colorStyles = {
+  cyan: 'border-cyan-500 text-cyan-500',
+  red: 'border-red-500 text-red-500',
+  green: 'border-green-500 text-green-500',
+  yellow: 'border-yellow-500 text-yellow-500',
+};
+
+const variantStyles = {
+  white: 'bg-white text-black',
+  filled: {
+    cyan: 'bg-cyan-500 text-white rounded-lg',
+    red: 'bg-red-500 text-white rounded-lg',
+    green: 'bg-green-500 text-white rounded-lg',
+    yellow: 'bg-yellow-500 text-white rounded-lg',
+  },
+};
+
+const Alert: React.FC<AlertProps> = ({ variant = 'white', color = 'cyan', title, icon, children }) => {
+  const variantStyle = variant === 'filled' ? variantStyles.filled[color] : variantStyles.white;
+  const colorStyle = variant === 'filled' ? '' : colorStyles[color];
+
+  return (
+    <div className={`${baseStyles} ${variantStyle} ${variant === 'white' && 'border-l-4'} ${colorStyle}`}>
+      <div className="mr-3 p-1">{icon}</div>
+      <div>
+        <h3 className="font-bold">{title}</h3>
+        <div>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Alert;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { Inter } from 'next/font/google';
 
-import Accordion from '@/examples/Accordion';
+import Alert from '@/examples/Alert';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <main className={` flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}>
       <div className="max-w-lg mx-auto mt-10">
-        <Accordion />
+        <Alert />
       </div>
     </main>
   );
