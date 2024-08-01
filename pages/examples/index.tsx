4c4196cfc4cfc46d613e40d19be6c116daac1ad7
ddoemonn <ozergokalpsezer@gmail.com>
2024-08-02 01:31:11 +0300

    FIX: other pages

--- pages/examples/index.tsx
+++ pages/examples/index.tsx
@@ -5,7 +5,9 @@ import Layout from '@/layout';
 export default function App() {
   return (
     <Layout>
-      <div className="max-w-xl mx-auto bg-black h-screen w-96" />
+      <div className="max-w-xl mx-auto h-screen w-96">
+        <h1 className="text-xl ">Examples Soon!</h1>
+      </div>
     </Layout>
   );
 }
--- pages/resources.tsx
+++ pages/resources.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import Layout from '@/layout';
+
 export default function App() {
-  return <div />;
+  return (
+    <Layout>
+      <div className="max-w-xl mx-auto  h-screen w-96">
+        <h1 className="text-xl ">Resources soon!</h1>
+      </div>
+    </Layout>
+  );
 }
