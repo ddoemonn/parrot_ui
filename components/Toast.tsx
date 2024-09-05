828e104afa7c35c31b533ed14463ba5977d36239
batuhanbaybas <b.baybas@gmail.com>
2024-09-05 10:07:52 +0300

    feat: global types created for animation props

--- components/Toast.tsx
+++ components/Toast.tsx
@@ -1,19 +1,11 @@
 import React, { useEffect } from 'react';
 
+import { Animation } from '@/types/globalTypes';
+
 type ToastProps = {
   message: string;
   duration?: number;
-  animation?:
-    | 'fade-in-left'
-    | 'fade-in'
-    | 'fade-in-right'
-    | 'fade-in-up'
-    | 'fade-in-down'
-    | 'slide-in-left'
-    | 'slide-in-right'
-    | 'slide-in-top'
-    | 'slide-in-bottom'
-    | 'bounce';
+  animation?: Animation;
   isVisible: boolean;
   onClose: () => void;
 };
